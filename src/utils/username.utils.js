0827cefddff967b961ba7295faba92dec684f82d
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-09-30 15:54:56 +0200

    ✨ Añadir utilidades de validación de usuario y cadenas

---
+++ src/utils/username.utils.js
@@ -0,0 +1,53 @@
+/**
+ * 
+ * @param {string} username 
+ * @param {Object} ctx
+ * @param {string} ctx.font
+ * @param {string} ctx.fillStyle
+ * @param {string} ctx.textAlign
+ * @param {TextMetrics} ctx.measureText
+ * @param {string} font 
+ * @param {string} size 
+ * @param {number} maxLength 
+ * @returns 
+ */
+function parseUsername(username, ctx, font, size, maxLength) {
+  username = username && username.replace(/\s/g, '') ? username : '?????'
+  let usernameChars = username.split('');
+  let editableUsername = '';
+  let finalUsername = '';
+
+  let newSize = +size;
+  let textLength;
+
+  let finalized = false;
+
+  while (!finalized) {
+    editableUsername = usernameChars.join('');
+
+    ctx.font = `${newSize}px ${font}`;
+    ctx.textAlign = 'left';
+    ctx.fillStyle = '#FFFFFF';
+
+    const actualLength = ctx.measureText(editableUsername).width;
+
+    if (actualLength >= maxLength) {
+      if (newSize > 60) newSize -= 1;
+      else usernameChars.pop();
+    }
+
+    if (actualLength <= maxLength) {
+      finalUsername = usernameChars.join('');
+      textLength = actualLength;
+      finalized = true;
+    }
+  }
+
+  return {
+    username: finalUsername,
+    newSize,
+    textLength,
+  };
+}
+
+module.exports = parseUsername;
