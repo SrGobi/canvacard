6a9a956f92f41bbb5af38d0ee316a7cec42566c4
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-09-30 15:53:54 +0200

    ✨ Añadir utilidades de validación de imágenes y cadenas

---
+++ src/utils/validations.utils.js
@@ -0,0 +1,57 @@
+const APIError = require("./error");
+
+function parseImg(imgString) {
+  if (!imgString || typeof imgString !== 'string') {
+    throw new APIError(`Invalid custom badge ('${imgString || undefined}') must be an image file 'png | jpg | jpeg | gif'`);
+  }
+  const URL = imgString.split('.')
+  const imgType = URL[URL.length - 1]
+  const imgCheck = /(jpg|jpeg|png|gif)/gi.test(imgType)
+
+  if (!imgCheck)
+    throw new APIError(`Invalid customBackground ('${imgString || undefined}') must be an image file 'png | jpg | jpeg | gif'`);
+
+  return imgString
+}
+
+function parsePng(imgString) {
+  if (!imgString || typeof imgString !== 'string') {
+    throw new APIError(`Invalid custom badge ('${imgString || undefined}') must be a png file`);
+  }
+  const URL = imgString.split('.')
+  const imgType = URL[URL.length - 1]
+  const imgCheck = /(png)/gi.test(imgType)
+
+  if (!imgCheck)
+    throw new APIError(`Invalid custom badge ('${imgString || undefined}') must be a png file`);
+
+  return imgString
+}
+
+function parseHex(hexString) {
+  const hexRegex = new RegExp(/^(#)?([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})$/)
+
+  if (!hexRegex.test(hexString))
+    throw new APIError(`Invalid Hex Code ('${hexString || undefined}')`);
+
+  if (!hexString.includes('#') && !hexString.startsWith('#'))
+    hexString = '#' + hexString
+
+  return hexString
+}
+
+function isString(param, type) {
+  if (typeof param !== 'string')
+    throw new APIError(`Invalid ${type} (${param || undefined}), must be a string`);
+
+  return param
+}
+
+function isNumber(param, type) {
+  if (isNaN(param))
+    throw new APIError(`Invalid ${type} (${param || undefined}), must be a number`);
+
+  return param
+}
+
+module.exports = { parseImg, parsePng, parseHex, isString, isNumber }
