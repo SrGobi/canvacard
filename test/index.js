43fcd86a471d46574243cc4b7501d1a187afa69b
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-10-15 16:09:56 +0200

    feat: Mas datos de testing

--- test/index.js
+++ test/index.js
@@ -1,34 +1,58 @@
 const canvacard = require("../index");
-const img = "https://cdn.discordapp.com/embed/avatars/0.png";
 
 // Datos de usuario para la tarjeta de Rank
 const userData = {
-	xp: 100500,
-	requiredXP: 100845
+	xp: 2100,
+	requiredXP: 3000
+};
+
+const data = {
+	id: '939986359697956874',
+	username: 'srgobi',
+	avatar: '73a9931eb6a7e430f394c513ade959e4',
+	discriminator: '0',
+	bot: true,
+	flags: 4194432,
+	banner:null,
+	accent_color: 3046143,
+	global_name: 'SrGobiawdawdawd',
+	avatar_decoration_data: {
+		asset: 'a_fed43ab12698df65902ba06727e20c0e',
+		sku_id: '1144058844004233369',
+		expires_at: null
+	},
+	banner_color: '#00ffbb',
+	clan: null,
+	createdTimestamp: 1572183145164,
+	defaultAvatarURL: 'https://cdn.discordapp.com/embed/avatars/0.png',
+	avatarURL: 'https://cdn.discordapp.com/avatars/360881334647914506/73a9931eb6a7e430f394c513ade959e4.png',
+	bannerURL: 'https://i.imgur.com/Z2g5IMl.gif'
 };
 
 // Función para generar la tarjeta de Rank
 async function generateRankCard() {
-	const rank = new canvacard.Rank()
-		.setAvatar(img)
+	const rank = new canvacard.Rank(data.id)
+		.setAvatar(data.avatarURL, data.avatar_decoration_data.asset, false)
+		.setBanner(data.bannerURL, true)
+		.setBadges(data.flags, data.bot, true)
 		.setCurrentXP(userData.xp)
 		.setRequiredXP(userData.requiredXP)
 		.setRank(1, "RANK", true)
-		.setLevel(1, "LEVEL", true)
-		.setStatus("dnd", true, 8)
+		.setLevel(20, "LEVEL")
+		.setStatus("online")
 		.setProgressBar(["#14C49E", "#FF0000"], "GRADIENT", true)
-		.setProgressBarTrack("#FFFFFF")
-		.setOverlay("#000000", 1, true)
-		.setUsername("SrGobi");
+		.setUsername(data.global_name, data.discriminator, "#FFFFFF")
+		.setCreatedTimestamp(data.createdTimestamp, "IMPRESIONANTE")
+		.setBorder(["#14C49E", "#FF0000"], "vertical");
 
-	const rankImage = await rank.build({ fontX: "Lobster", fontY: "Lobster" });
-	canvacard.write(rankImage, "./card.png");
+	const rankImage = await rank.build("Cascadia Code PL");
+	canvacard.write(rankImage, "./card_1.png");
 }
 
 // Función para generar la tarjeta de Welcome
 async function generateWelcomeCard() {
 	const welcome = new canvacard.Welcomer()
-		.setAvatar(img)
+		.setAvatar(data.defaultAvatarURL)
 		.setBackground('IMAGE', 'https://i.imgur.com/aClDVjh.jpg')
 		.setTitulo("Titulo de la Tarjeta👋")
 		.setSubtitulo("Subtitulo de la Tarjeta 👋")
@@ -43,33 +67,25 @@ async function generateWelcomeCard() {
 	canvacard.write(welcomeImage, "./welcomer.png");
 }
 
-// Función para generar la tarjeta de Perfil
-async function generateProfileCard() {
-	const profile = new canvacard.ProfileImage("939986359697956874", {
-		badgesFrame: true,
-		moreBackgroundBlur: true,
-		presenceStatus: 'online',
-		rankData: {
-			currentXp: 2100,
-			requiredXp: 3000,
-			rank: 1,
-			level: 20,
-			barColor: '#fcdce1',
-			levelColor: '#ada8c6',
-			autoColorRank: true
-		}
-	});
-	await profile.fetchData();
+// Función para generar la tarjeta de Spotify
+async function generateSpotifyCard() {
+	const spotify = new canvacard.Spotify()
+		.setAuthor("SAIKO")
+		.setAlbum("SAKURA")
+		.setStartTimestamp(Date.now() - 10000)
+		.setEndTimestamp(Date.now() + 50000)
+		.setImage("https://i.scdn.co/image/ab67616d00001e02e346fc6f767ca2ac8365fe60")
+		.setTitle("YO LO SOÑÉ");
 
-	const profileImage = await profile.build("Cascadia Code PL");
-	canvacard.write(profileImage, "./profile.png");
+	const spotifyImage = await spotify.build("Cascadia Code PL");
+	canvacard.write(spotifyImage, "./spotify.png");
 }
 
 // Función principal para ejecutar todas las generaciones
 async function main() {
 	await generateRankCard();
 	await generateWelcomeCard();
-	await generateProfileCard();
+	await generateSpotifyCard();
 }
 
 main().catch(console.error);
