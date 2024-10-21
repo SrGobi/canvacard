6da32e23327f376c1ba72bdd42814a7d7dcbed09
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-10-21 22:09:50 +0200

    feat: Actualizar función getIconPath para obtener la URL absoluta del ícono
    La función getIconPath en el archivo icon.utils.js ha sido actualizada para obtener la URL absoluta del ícono de una insignia. Si el ícono es una ruta relativa, se transforma en una URL absoluta apuntando al repositorio de GitHub. Esto permite obtener la URL correcta del ícono en cualquier contexto. Si la URL ya es absoluta, se devuelve sin cambios.

--- src/utils/icon.utils.js
+++ src/utils/icon.utils.js
@@ -1,15 +1,23 @@
 const APIError = require('./error.utils');
-const path = require('path');
+
 /**
- * Función para construir la ruta del archivo SVG.
- * @param {string} icon 
- * @returns {string} Ruta del archivo SVG
+ * Función para obtener la URL del ícono de una insignia.
+ * Si el ícono es una ruta relativa, se transforma en una URL absoluta apuntando al repositorio de GitHub.
+ * 
+ * @param {string} iconUrl - Nombre del ícono o URL del ícono.
+ * @returns {string} URL absoluta del ícono.
+ * @throws {APIError} Si hay un problema al construir la URL del ícono.
  */
-function getIconPath(icon) {
+function getIconPath(iconUrl) {
   try {
-    return path.join(__dirname, '../../assets/flags', icon);
+    // Verifica si la URL es absoluta, si no lo es, convierte la ruta en una URL remota válida.
+    if (!iconUrl.startsWith('http')) {
+      // Devuelve la URL completa ajustando la ruta base al repositorio de GitHub.
+      return `https://raw.githubusercontent.com/SrGobi/canvacard/assets/images/flags/icons/${iconUrl}`;
+    }
+    return iconUrl;
   } catch (error) {
-    throw new APIError(`Error constructing icon path: ${error.message}`);
+    throw new APIError(`Error constructing icon URL: ${error.message}`);
   }
 }
 
