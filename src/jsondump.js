f5a9e9de43c8674a9d24db08ebc0b328cdc1b5d4
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-12-02 18:56:39 +0100

    feat: Añadir soporte para archivos de fuentes en la generación de datos JSON

--- src/jsondump.js
+++ src/jsondump.js
@@ -6,36 +6,54 @@ import { performance } from "node:perf_hooks";
 const start_time = performance.now();
 const date = new Date();
 const imgDir = path.resolve("./images");
+const fontsDir = path.resolve("./fonts");
 const GH_RAW_URL_BASE = "https://raw.githubusercontent.com/SrGobi/canvacard/assets";
 const GH_URL_BASE = "https://github.com/SrGobi/canvacard/blob/assets";
 
 console.log("Building JSON data of assets");
 const imageFiles = await fs.readdir(imgDir).then(res => {
-    return Promise.all(res.map(async m => {
-        const stat = await fs.stat(path.join(imgDir, m));
-        return {
-            name: m,
-            size: stat.size,
-            size_formatted: `${(stat.size / (1024 * 1024)).toFixed(2)} MB`,
-            file_name: m.replace(path.extname(m), ""),
-            ext: path.extname(m),
-            url: `${GH_RAW_URL_BASE}/images/${m}`,
-            file_url: `${GH_URL_BASE}/images/${m}`
-        };
-    }));
+	return Promise.all(res.map(async m => {
+		const stat = await fs.stat(path.join(imgDir, m));
+		return {
+			name: m,
+			size: stat.size,
+			size_formatted: `${(stat.size / (1024 * 1024)).toFixed(2)} MB`,
+			file_name: m.replace(path.extname(m), ""),
+			ext: path.extname(m),
+			url: `${GH_RAW_URL_BASE}/images/${m}`,
+			file_url: `${GH_URL_BASE}/images/${m}`
+		};
+	}));
+});
+
+const fontFiles = await fs.readdir(fontsDir).then(res => {
+	return Promise.all(res.map(async m => {
+		const stat = await fs.stat(path.join(fontsDir, m));
+		return {
+			name: m,
+			size: stat.size,
+			size_formatted: `${(stat.size / (1024 * 1024)).toFixed(2)} MB`,
+			file_name: m.replace(path.extname(m), ""),
+			ext: path.extname(m),
+			url: `${GH_RAW_URL_BASE}/fonts/${m}`,
+			file_url: `${GH_URL_BASE}/fonts/${m}`
+		};
+	}));
 });
 
 const datasrc = {
-    id: crypto.randomUUID(),
-    images: imageFiles,
-    total_files: imageFiles.length,
-    images_count: imageFiles.length,
-    github: "https://github.com/SrGobi/canvacard",
-    website: "https://canvacard.js.org",
-    npm: "https://www.npmjs.com/package/canvacard",
-    generated_timestamp: date.getTime(),
-    generated_timestamp_iso: date.toISOString(),
-    shasum: "https://raw.githubusercontent.com/SrGobi/canvacard/assets/shasum.txt"
+	id: crypto.randomUUID(),
+	images: imageFiles,
+	fonts: fontFiles,
+	total_files: imageFiles.length + fontFiles.length,
+	images_count: imageFiles.length,
+	fonts_count: fontFiles.length,
+	github: "https://github.com/SrGobi/canvacard",
+	website: "https://canvacard.js.org",
+	npm: "https://www.npmjs.com/package/canvacard",
+	generated_timestamp: date.getTime(),
+	generated_timestamp_iso: date.toISOString(),
+	shasum: "https://raw.githubusercontent.com/SrGobi/canvacard/assets/shasum.txt"
 };
 
 const datasrc_stringified = JSON.stringify(datasrc);
