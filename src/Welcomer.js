3028bb371dc7fdb08b2a51695ea7746823a1bf63
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-02-24 12:30:18 +0100

    feat: Añadir soporte para registro de fuentes en Welcomer y mejorar la inicialización de datos

--- src/Welcomer.js
+++ src/Welcomer.js
@@ -1,9 +1,10 @@
 const { Base } = require("discord-canvascard");
 const Util = require("./Util");
+const Canvas = require('canvas');
+const assets = require('./Assets');
 
 class Welcomer extends Base {
-
-    /**
+	/**
      * Welcome image builder
      * @see https://www.discord-canvas.net/functions/welcome
      * @example
@@ -17,81 +18,113 @@ class Welcomer extends Base {
                 canvacard.write(data, "welcomer.png");
             })
      */
-    constructor() {
-        super();
-        
-        /**
-         * Título creado con Canvacard
-         * @type {string}
-         */
-        this.titulo = "WELCOME";
+	constructor() {
+		super();
 
-        /**
-         * Mensaje de texto creado con Canvacard
-         * @type {string}
-         */
-        this.subtitulo = "Welcome to {server}";
+		/**
+		 * Título creado con Canvacard
+		 * @type {string}
+		 */
+		this.titulo = 'WELCOME';
 
-        /**
-         * Color del título creado con canvacard
-         * @type {string}
-         */
-        this.colorTitulo = "#03A9F4";
-        
-        // actualizar datos predeterminados
-        this.__updateData();
-    }
+		/**
+		 * Mensaje de texto creado con Canvacard
+		 * @type {string}
+		 */
+		this.subtitulo = 'Welcome to {server}';
 
-    /**
-     * Actualiza el estado predeterminado
-     * @private
-     * @ignore
-     */
-     __updateData() {
-        this.setAvatar(`https://cdn.discordapp.com/embed/avatars/0.png`);
-        this.setBackground("COLOR", "#2c2f33" ||"IMAGE", `https://i.imgur.com/aClDVjh.jpg`);
-        this.setTitulo("Titulo personalizable!");
-        this.setSubtitulo("Subtitulo personalizable!");
-        this.setColorTitulo("#FFFFFF");
-        this.setColorSubtitulo("#5865f2");
-        this.setColorCircle("#FFFFFF");
-        this.setColorBorder("#000000");
-        this.setOpacityBorder("0.4");
-        this.setColor("border", "#4D5E94");
-        this.setColor("titulo", "#4D5E94");
-        this.setColor("subtitulo", "#4D5E94");
-        this.setColor("avatar", "#4D5E94");
-    }
+		/**
+		 * Color del título creado con canvacard
+		 * @type {string}
+		 */
+		this.colorTitulo = '#03A9F4';
 
-    /**
-     * Establecer color
-     * @param {"titulo"|"titulo-border"|"titulo-box"|"subtitulo"|"subtitulo-border"|"subtitulo-box"|"avatar"|"background"|"border"} id
-     * @param {string} color Código de color HTML5
-     * @returns {Welcomer}
-     */
-    setColor(id, color) {
-        super.setColor(id, color);
-        return this;
-    }
+		// Cargar fuentes predeterminadas
+		this.registerFonts();
 
-    /**
-     * Establecer recuento de miembros
-     * @param {number|string} memberCount Recuento de miembros del gremio
-     * @returns {Welcomer}
-     */
-    setMemberCount(memberCount = 100) {
-        super.setMemberCount(Util.toAbbrev(memberCount));
-        return this;
-    }
+		// actualizar datos predeterminados
+		this.__updateData();
+	}
 
-    /**
-     * Crea una imagen de bienvenida
-     * @returns {Promise<Buffer>}
-     */
-    async build() {
-        return (await this.toAttachment()).toBuffer();
-    }
+	/**
+	 * Fuente Loads
+	 * @param {any[]} fontArray Matriz de fuentes
+	 * @returns {Welcomer}
+	 */
+	registerFonts(fontArray = []) {
+		if (!fontArray.length) {
+			setTimeout(() => {
+				// default fonts
+				Canvas.registerFont(assets.font.get('UNI_SANS'), {
+					family: 'Sans Heavy',
+					weight: 'bold',
+					style: 'normal'
+				});
 
+				Canvas.registerFont(assets.font.get('UNI_SANS'), {
+					family: 'Sans Heavy',
+					weight: 'regular',
+					style: 'normal'
+				});
+			}, 250);
+		} else {
+			fontArray.forEach((font) => {
+				Canvas.registerFont(font.path, font.face);
+			});
+		}
+
+		return this;
+	}
+
+	/**
+	 * Actualiza el estado predeterminado
+	 * @private
+	 * @ignore
+	 */
+	__updateData() {
+		this.setAvatar(`https://cdn.discordapp.com/embed/avatars/0.png`);
+		this.setBackground('COLOR', '#2c2f33' || 'IMAGE', `https://i.imgur.com/aClDVjh.jpg`);
+		this.setTitulo('Titulo personalizable!');
+		this.setSubtitulo('Subtitulo personalizable!');
+		this.setColorTitulo('#FFFFFF');
+		this.setColorSubtitulo('#5865f2');
+		this.setColorCircle('#FFFFFF');
+		this.setColorBorder('#000000');
+		this.setOpacityBorder('0.4');
+		this.setColor('border', '#4D5E94');
+		this.setColor('titulo', '#4D5E94');
+		this.setColor('subtitulo', '#4D5E94');
+		this.setColor('avatar', '#4D5E94');
+	}
+
+	/**
+	 * Establecer color
+	 * @param {"titulo"|"titulo-border"|"titulo-box"|"subtitulo"|"subtitulo-border"|"subtitulo-box"|"avatar"|"background"|"border"} id
+	 * @param {string} color Código de color HTML5
+	 * @returns {Welcomer}
+	 */
+	setColor(id, color) {
+		super.setColor(id, color);
+		return this;
+	}
+
+	/**
+	 * Establecer recuento de miembros
+	 * @param {number|string} memberCount Recuento de miembros del gremio
+	 * @returns {Welcomer}
+	 */
+	setMemberCount(memberCount = 100) {
+		super.setMemberCount(Util.toAbbrev(memberCount));
+		return this;
+	}
+
+	/**
+	 * Crea una imagen de bienvenida
+	 * @returns {Promise<Buffer>}
+	 */
+	async build() {
+		return (await this.toAttachment()).toBuffer();
+	}
 }
 
 module.exports = Welcomer;
