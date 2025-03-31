7e01a5f9f9ba2d0b38ba53503be7d8936957de98
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-03-31 11:21:15 +0200

    feat: Añadir ImageFactory para gestionar URLs de imágenes en Canvacard

---
+++ src/AssetsFactory.js
@@ -0,0 +1,48 @@
+const BASE_URL =
+  process.env.CANVACORD_ASSETS_BASE_URL ||
+  "https://raw.githubusercontent.com/SrGobi/canvacard/assets/images";
+
+/**
+ * Prepares the full URL for an image.
+ * @param {string} path - The name of the image.
+ * @returns {string} - Full image URL.
+ * @private
+ */
+const prepareURL = (path) => `${BASE_URL}/${path}`;
+
+/**
+ * ImageFactory: Provides URLs for data (images or json documents) that can be used in Canvacard.
+ */
+const ImageFactory = {
+  BATSLAP: prepareURL("BATSLAP.png"),
+  BED: prepareURL("BED.png"),
+  CLYDE: prepareURL("CLYDE.png"),
+  DELETE: prepareURL("DELETE.png"),
+  DISCORD: prepareURL("DISCORD.png"),
+  DISTRACTED: prepareURL("DISTRACTED.png"),
+  FACEPALM: prepareURL("FACEPALM.png"),
+  IMPOSTER: prepareURL("IMPOSTER.png"),
+  IMPOSTERWITH: prepareURL("IMPOSTERWITH.png"),
+  JAIL: prepareURL("JAIL.png"),
+  KISS: prepareURL("KISS.png"),
+  KO: prepareURL("KO.png"),
+  PHUB: prepareURL("PHUB.png"),
+  PUNCH: prepareURL("PUNCH.png"),
+  SNOWFLAKE: prepareURL("SNOWFLAKE.png"),
+  SPANK: prepareURL("SPANK.png"),
+  TOBECONTINUED: prepareURL("TOBECONTINUED.png"),
+  TRIGGERED: prepareURL("TRIGGERED.png"),
+  WANTED: prepareURL("WANTED.png"),
+  WASTED: prepareURL("WASTED.png"),
+  YOUTUBE: prepareURL("YOUTUBE.png"),
+  FLAGS: {
+    ICONS: {
+      USERNAME: prepareURL("flags/icons/username.png"),
+    },
+    APPLICATION: prepareURL("flags/application.json"),
+    USER: prepareURL("flags/user.json"),
+  }
+
+};
+
+module.exports = { ImageFactory };
---
+++ typings/src/AssetsFactory.d.ts
@@ -0,0 +1,31 @@
+export namespace ImageFactory {
+    let BATSLAP: string;
+    let BED: string;
+    let CLYDE: string;
+    let DELETE: string;
+    let DISCORD: string;
+    let DISTRACTED: string;
+    let FACEPALM: string;
+    let IMPOSTER: string;
+    let IMPOSTERWITH: string;
+    let JAIL: string;
+    let KISS: string;
+    let KO: string;
+    let PHUB: string;
+    let PUNCH: string;
+    let SNOWFLAKE: string;
+    let SPANK: string;
+    let TOBECONTINUED: string;
+    let TRIGGERED: string;
+    let WANTED: string;
+    let WASTED: string;
+    let YOUTUBE: string;
+    namespace FLAGS {
+        namespace ICONS {
+            let USERNAME: string;
+        }
+        let APPLICATION: string;
+        let USER: string;
+    }
+}
+//# sourceMappingURL=AssetsFactory.d.ts.map
