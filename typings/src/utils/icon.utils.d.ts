ac4d4102515f62720d82f72f7faf633d45eeb096
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-03-31 18:52:57 +0200

    feat: Añadir función getIconPath para obtener la URL de un ícono de insignia

---
+++ typings/src/utils/icon.utils.d.ts
@@ -0,0 +1,10 @@
+export = getIconPath;
+/**
+ * @name getIconPath
+ * @description Function to get the URL of a badge icon. If the icon is a relative path, it is transformed into an absolute URL pointing to the GitHub repository.
+ * @param {string} iconUrl Icon name or icon URL.
+ * @returns {string} Absolute URL of the icon.
+ * @throws {APIError} If there is a problem building the icon URL.
+ */
+declare function getIconPath(iconUrl: string): string;
+//# sourceMappingURL=icon.utils.d.ts.map
