0eaa481f14c8ff3dc18108f916da2c0ca4e6203d
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-03-31 11:11:50 +0200

    feat: Agregar función formatAndValidateHex para formatear y validar códigos hexadecimales

---
+++ typings/src/utils/formatAndValidateHex.utils.d.ts
@@ -0,0 +1,10 @@
+export = formatAndValidateHex;
+/**
+ * @name formatAndValidateHex
+ * @description Formats and validates the hexadecimal code.
+ * @param {string} hex Hexadecimal code for formatting and validation
+ * @param {string} alt Alternative color if code is invalid
+ * @returns {string} Formatted and validated hexadecimal code
+ */
+declare function formatAndValidateHex(hex: string, alt?: string): string;
+//# sourceMappingURL=formatAndValidateHex.utils.d.ts.map
