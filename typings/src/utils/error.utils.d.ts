28d98fb6b82a882fd847b38c572bef1724d15af2
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-03-31 11:03:34 +0200

    feat: Agregar clase APIError para manejar errores de API

---
+++ typings/src/utils/error.utils.d.ts
@@ -0,0 +1,13 @@
+export = APIError;
+/**
+ * APIError class
+ * @class APIError
+ * @extends {Error}
+ * @classdesc Error class for API errors
+ * @param {string} message Error message
+ * @returns {APIError} APIError object
+ */
+declare class APIError extends Error {
+    constructor(message: any);
+}
+//# sourceMappingURL=error.utils.d.ts.map
