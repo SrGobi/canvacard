d509f541f2cb1f5c7033ced1d96bc8477e4cf283
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-03-31 11:12:16 +0200

    feat: Agregar función formatVariable para formatear variables y tipos

---
+++ typings/src/utils/formatVariable.utils.d.ts
@@ -0,0 +1,10 @@
+export = formatVariable;
+/**
+ * @name formatVariable
+ * @description Gets variables and types.
+ * @param {object} prefix The type of variable
+ * @param {object} variable The variable to change
+ * @returns {string} The formatted variable
+ */
+declare function formatVariable(prefix: object, variable: object): string;
+//# sourceMappingURL=formatVariable.utils.d.ts.map
